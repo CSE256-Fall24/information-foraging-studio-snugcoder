5015f51278edc2a0814e5c8b575522f26437b1a4
adejolaO <aogunsan@cmitsouth.org>
2024-09-12 14:23:11 -0500

    Completed Studio - Completed Sticky Header

--- scripts/ts/dynamic-dom/do-something.js
+++ scripts/ts/dynamic-dom/do-something.js
@@ -1,3 +1,15 @@
 export function doSomething() {
+  window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("main-menu-container");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.scrollY > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
   console.log("Put some code in here!");
 }
